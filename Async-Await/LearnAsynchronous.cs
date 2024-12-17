f22a16f32c77429396e18ac389ed628ed4ec2e2d
asus <mha672001@yahoo.com>
2024-12-17 21:20:39 +0330

    Add project files.

---
+++ Async-Await/LearnAsynchronous.cs
@@ -0,0 +1,17 @@
+﻿namespace Async_Await
+{
+    public class LearnAsynchronous
+    {
+        public void MainMethod()
+        {
+            DelayedMethod();
+            Console.WriteLine("Number 2");
+        }
+
+        public async void DelayedMethod()
+        {
+            await Task.Delay(5000);                      
+            Console.WriteLine("Number 1");
+        }
+    }
+}
---
+++ Async-Await/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Async_Await
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var learnAsynchronous = new LearnAsynchronous();
+            learnAsynchronous.MainMethod();
+            Console.ReadKey();
+        }
+        
+    }
+
+}
